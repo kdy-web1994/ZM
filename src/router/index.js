fae432074c0239645a379191e8212c5bd9fd19ae
muzhangxion <254497987@qq.com>
2019-04-20 09:36:12 +0800

    login页

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 // import HelloWorld from '@/views/HelloWorld'
 
+
 Vue.use(Router)
 
 export default new Router({
@@ -15,6 +16,11 @@ export default new Router({
       path: '/Im',
       name: 'Im',
       component:()=>import(/* webpackChunkName: "Im" */ '@/views/Im')
+    },
+    {
+      path: '/Login',
+      name: 'Login',
+      component:()=>import(/* webpackChunkName: "Im" */ '@/page/login/login')
     }
   ]
 })
