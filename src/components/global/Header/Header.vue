1a09e1f0f7f097e66168714efba41763e26e6711
kdy-web1994 <45192275+kdy-web1994@users.noreply.github.com>
2019-04-21 15:01:01 +0800

    header footer

---
+++ src/components/global/Header/Header.vue
@@ -0,0 +1,16 @@
+<template>
+    <div id="Header">
+      Header组件
+    </div>
+  </template>
+  <script>
+  export default {
+    name: 'Header'
+  }
+  </script>
+  <style lang="scss" scoped>
+  .Header {
+  
+  }
+  </style>
+  
