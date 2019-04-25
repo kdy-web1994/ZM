e7d7596ff6336e721407701ebfe678d515476d3d
kdy-web1994 <45192275+kdy-web1994@users.noreply.github.com>
2019-04-25 10:23:03 +0800

    News components

---
+++ src/components/global/News/News.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="News">
+        <div class="number">12</div>
+    </div>
+  </template>
+  <script>
+  export default {
+    name: 'News'
+  }
+  </script>
+  <style lang="scss" scoped>
+  .News {
+    position: fixed;
+    z-index: 100;
+    width:0.88rem;
+    height: 0.88rem;
+    border-radius:50%;
+    bottom: 1.23rem;
+    right: 0.22rem; 
+    background: url("../../../assets/home_icon_customer_service@3x.png") no-repeat;
+    background-size:100% 100%;
+       .number{
+         position: absolute;
+        //  width: 0.3rem;
+        box-sizing: border-box;
+         font-size: 0.32rem;
+         color: #fff;
+         transform: scale(0.5);
+         padding: 0.1rem 0.14rem ;
+         top: -0.13rem;
+         right: -0.2rem;
+         z-index: 110;
+         background: #FF2525;
+         border-radius:0.18rem;
+       }
+  }
+  </style>
+  
--- src/page/main/service.vue
+++ src/page/main/service.vue
@@ -13,6 +13,7 @@
              </div>
          </div>
      </div>
+     <News />
   </div>
 </template>
   <script>
